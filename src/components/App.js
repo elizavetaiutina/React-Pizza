a9c33317fb2dd1e64ea9a6172602ae4647233f94
Liza Iutina <iutinaelizaveta@yandex.ru>
2023-02-20 18:27:08 +0300

    feat: add style scss, components(header, categories, sort, pizzablock)

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import Header from "./Header";
+import Categories from "./Categories";
+import Sort from "./Sort";
+import PizzaBlock from "./PizzaBlock";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="Мексиканская" price={350} />
+            <PizzaBlock title="Пепперони" price={325} />
+            <PizzaBlock title="Чизбургер-пицца" price={395} />
+            <PizzaBlock title="Пепперони" price={325} />
+            <PizzaBlock title="Мексиканская" price={350} />
+            <PizzaBlock title="Пепперони" price={325} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
