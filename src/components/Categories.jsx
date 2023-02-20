2769290b4fe22282270674f8ddc684b5ac7af606
Liza Iutina <iutinaelizaveta@yandex.ru>
2023-02-20 21:26:56 +0300

    feat: render list categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,28 @@
+import React from "react";
+
 function Categories() {
+  const categories = ["Все", "Мясные", "Вегетарианская", "Гриль", "Острые", "Закрытые"];
+
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const clickActive = (index) => {
+    setActiveIndex(index);
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((categor, index) => {
+          return (
+            <li
+              key={index}
+              onClick={() => clickActive(index)}
+              className={activeIndex == index ? "active" : ""}
+            >
+              {categor}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
