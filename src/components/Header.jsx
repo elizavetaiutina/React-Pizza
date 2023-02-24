a4db10ec21ef1bd79c98e69df0a268ecb93b5311
Liza Iutina <iutinaelizaveta@yandex.ru>
2023-02-24 19:24:52 +0300

    feat: инпут поиска и адаптив

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,24 @@
 import { Link } from "react-router-dom";
 
 import logoSvg from "../assets/img/pizza-logo.svg";
+import Search from "./Search/index.jsx";
 
 function Header() {
   return (
     <div className="header">
       <div className="container">
-        <Link to="/">
-          <div className="header__logo">
-            <img width="38" src={logoSvg} alt="Pizza logo" />
-            <div>
-              <h1>React Pizza</h1>
-              <p>самая вкусная пицца во вселенной</p>
+        <div className="wrapper">
+          <Link to="/">
+            <div className="header__logo">
+              <img width="38" src={logoSvg} alt="Pizza logo" />
+              <div>
+                <h1>React Pizza</h1>
+                <p>самая вкусная пицца во вселенной</p>
+              </div>
             </div>
-          </div>
-        </Link>
+          </Link>
+          <Search />
+        </div>
         <div className="header__cart">
           <Link to="/cart" className="button button--cart">
             <span>520 ₽</span>
